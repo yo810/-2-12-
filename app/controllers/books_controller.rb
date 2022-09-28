8f84fca8b0df97c73ea56848ce29fea9ae741ff9
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-09-28 21:38:39 +0000

    [2-12] サクセスメッセージの装着

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -25,6 +30,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
