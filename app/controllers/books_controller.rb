2243205baa6fa744132952c9a3a06e406eb67862
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-09-29 23:18:25 +0000

    [2-12] エラー修正、レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book)
     else
-      @books = Book.all
-      render :index
+    @books = Book.all
+    render :index
     end
   end
 
@@ -28,10 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
